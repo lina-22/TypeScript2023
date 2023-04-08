14a64969645ff559e577ee643c1f8c3411484e09
lina-22 <haquefahinur@gmail.com>
2023-04-08 15:53:50 +0200

    first commit

---
+++ v1-v3-30min/index.js
@@ -0,0 +1,5 @@
+function addNumbers(num1, num2) {
+    console.log(num1 + num2);
+}
+addNumbers(20, 30);
+// tsc index.ts->index.js->node index.js
---
+++ v1-v3-30min/index.ts
@@ -0,0 +1,7 @@
+function addNumbers(num1: Number, num2: Number) {
+  console.log(num1 + num2);
+}
+
+addNumbers(20, 30);
+
+// tsc index.ts->index.js->node index.js
