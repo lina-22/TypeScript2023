3f974881a6fc66fde1037562aac7256bed1271cf
lina-22 <haquefahinur@gmail.com>
2023-04-23 23:02:52 +0200

    starting the typescript course

--- v1-v3-30min/index.js
+++ v1-v3-30min/index.js
@@ -1,5 +1,7 @@
-function addNumbers(num1, num2) {
-    console.log(num1 + num2);
-}
-addNumbers(20, 30);
+//function addNumbers(num1: Number, num2: Number) {
+//console.log(num1 + num2);
+//}
+//addNumbers(20, 30);
 // tsc index.ts->index.js->node index.js
+var userName = "Fahinur Haque";
+console.log(userName);
--- v1-v3-30min/index.ts
+++ v1-v3-30min/index.ts
@@ -1,7 +1,10 @@
-function addNumbers(num1: Number, num2: Number) {
-  console.log(num1 + num2);
-}
+//function addNumbers(num1: Number, num2: Number) {
+//console.log(num1 + num2);
+//}
 
-addNumbers(20, 30);
+//addNumbers(20, 30);
 
 // tsc index.ts->index.js->node index.js
+
+let userName = "Fahinur Haque";
+console.log(userName);
